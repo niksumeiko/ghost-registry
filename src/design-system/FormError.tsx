bd23a7ae5d6a5e69c67a2714fc6584f16ebc446b
Nik Sumeiko <nik.sumeiko@gmail.com>
2026-06-12 22:43:32 +0200

    refactor: Assing alert role to <FormError /> (#4)

--- src/design-system/FormError.tsx
+++ src/design-system/FormError.tsx
@@ -2,7 +2,10 @@ import { PropsWithChildren } from 'react';
 
 export function FormError({ children }: PropsWithChildren) {
     return (
-        <div className="bg-red-200 px-4 py-3 flex items-center gap-2 my-3 text-sm">
+        <div
+            role="alert"
+            className="bg-red-200 px-4 py-3 flex items-center gap-2 my-3 text-sm"
+        >
             <span aria-hidden>✘</span>
             {children}
         </div>
